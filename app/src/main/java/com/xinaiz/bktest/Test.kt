a92bf9f40b27d53fc82d459c23e615f7ffa2f88e
xinaiz <dominic.jurczak@yahoo.com>
2018-09-02 05:01:47 -0700

    module configuration

---
+++ app/src/main/java/com/xinaiz/bktest/Test.kt
@@ -0,0 +1,11 @@
+package com.xinaiz.bktest
+
+import com.xinaiz.betterkotlin.add
+
+/**
+ * Created by Altconnect on 9/2/2018.
+ */
+
+fun main(args: Array<String>) {
+    print(add(3, 4))
+}
---
+++ betterkotlin/src/main/java/com/xinaiz/betterkotlin/Core.kt
@@ -0,0 +1,9 @@
+package com.xinaiz.betterkotlin
+
+/**
+ * Created by Altconnect on 9/2/2018.
+ */
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
